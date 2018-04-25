1ac2b04c3f6f91001dd755cbb1394ce40cc6b452
Bogdan Dinga <bogdan.dinga@e-spres-oh.com>
2018-04-25 04:04:54 +0300

    Programmatic injection

--- popup.js
+++ popup.js
@@ -1,11 +1,21 @@
 function onLoad() {
   const name = document.getElementsByTagName('span')[0];
+  const removeLogoButton = document.getElementById('remove-logo');
+
   chrome.storage.sync.get(['tim_js_name'], items => {
     if (items.tim_js_name) {
       name.innerText = `${items.tim_js_name}`;
     } else {
       name.innerText = `tim.js`;
     }
+  });
+
+  removeLogoButton.addEventListener('click', removeLogo, true);
+}
+
+function removeLogo() {
+  chrome.tabs.executeScript({
+    code: `document.getElementById('ext-container').remove()`
   });
 }
 
